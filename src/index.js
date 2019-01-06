f030fccea8879c77d5cb3bcd061df731c7b48f33
chicken ahn <chiwanan91@gmail.com>
2019-01-06 19:05:58 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+    user: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Error {
+    field: String!
+    message: String!
+  }
+  type RegisterResponse {
+    errors: [Error!]!
+    user: User
+  }
+
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): Boolean!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => null,
+    user: () => ({
+      id: 1,
+      username: 'bob'
+    })
+  },
+  Mutation: {
+    login: () => true,
+    register: () => ({
+      errors: [{ field: 'username', message: 'bad' }],
+      user: { id: 1, username: 'bob' }
+    })
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => console.log(`server started at ${url}`))
